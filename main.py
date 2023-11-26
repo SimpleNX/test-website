be6d58db54c31c8233b2822feaddcb5e22219c60
Simple Panigrahi <simplepanigrahi17@gmail.com>
2023-11-26 17:28:53 +0000

    Test Run Successful!

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "<p>Test Successful!</p>"
+def home():
+  return render_template('index.html')
 
-app.run(debug=True)
+app.run(debug=True, port=5001)
